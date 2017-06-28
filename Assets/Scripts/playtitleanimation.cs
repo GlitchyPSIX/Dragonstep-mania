8db8f9248ccd6e7225baeb00fd2661e464eae7bc
José Antonio <supermglitchy3@gmail.com>
2017-06-28 13:05:25 -0400

    oopsies

--- Assets/Scripts/playtitleanimation.cs
+++ Assets/Scripts/playtitleanimation.cs
@@ -8,7 +8,7 @@ public class playtitleanimation : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Step"){
+		if (Input.GetButtonDown("Step")){
 			GetComponent<Animator>().Play("Draw Out");
 		}
 		//Visual Studio Code acted super dense for me today.

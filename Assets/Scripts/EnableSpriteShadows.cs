edb0603ff3e1514fe346166a2a8e362fae053b1b
José Antonio <supermglitchy3@gmail.com>
2017-07-01 02:38:49 -0400

    remove unused stuff, start working in the intro sequence (Started with the models, super simple yet they work.)

---
+++ Assets/Scripts/EnableSpriteShadows.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableSpriteShadows : MonoBehaviour {
+
+public bool CastShadows;
+public bool ReceiveShadows;
+public SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<SpriteRenderer>();
+		renderer.castShadows = CastShadows;
+	}
+
+}

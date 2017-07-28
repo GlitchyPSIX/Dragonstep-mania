15e665d40d62b81a49560ae2bf049b4dfe6af3f7
José Antonio <supermglitchy3@gmail.com>
2017-07-27 23:47:56 -0400

    working on the title screen

--- Assets/Scripts/playtitleanimation.cs
+++ Assets/Scripts/playtitleanimation.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class playtitleanimation : MonoBehaviour {
 
+    AsyncOperation LoadDebug;
+    Text VersionText;
 
+    void Start(){
+		LoadDebug = SceneManager.LoadSceneAsync("debug");
+        VersionText = GameObject.Find("VersionTag").GetComponent<Text>();
+        VersionText.text = "Loading...";
+    }
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown("Step")){
 			GetComponent<Animator>().Play("Draw Out");
 		}
-		
-		//Visual Studio Code acted super dense for me today.
+
 	}
 }

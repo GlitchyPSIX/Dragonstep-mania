6739e2b7244d811dc4d7e8c0655aef78547eca47
José Antonio <supermglitchy3@gmail.com>
2017-11-21 15:04:05 -0400

    Preparing the base for endless mode

--- Assets/Scripts/Conductor.cs
+++ Assets/Scripts/Conductor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Conductor : MonoBehaviour
 {
@@ -9,19 +10,27 @@ public class Conductor : MonoBehaviour
     //thanks, fizzd, I owe you a big one.
 
     public float bpm = 120;
-    public float offset = 0.2f;
+    public float offset = 0;
     public double songposition;
+    double dspOffset = 0d;
     public AudioSource song;
+
+    public bool trainingMode;
 
     // Use this for initialization
     void Start()
     {
         song = GetComponent<AudioSource>();
+        resetStartOfSong();
     }
 
     // Update is called once per frame
     void Update()
     {
-        songposition = (double)(song.time) * song.pitch - offset;
+        songposition = ((double)(AudioSettings.dspTime) - dspOffset) * song.pitch - offset;
+    }
+
+    public void resetStartOfSong(){
+        dspOffset = AudioSettings.dspTime;
     }
 }

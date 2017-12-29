db4b049b39c338733683101d3da72ba2cdc6fcbe
GlitchyPSIX <supermglitchy3@gmail.com>
2017-12-29 17:06:33 -0400

    Add sample demo acknowledgance dialog

--- Assets/Scripts/UI/DialogControl.cs
+++ Assets/Scripts/UI/DialogControl.cs
@@ -19,70 +19,87 @@ public class DialogControl : MonoBehaviour
     AudioSource buttonLSFX;
     AudioSource buttonMSFX;
     AudioSource buttonRSFX;
-    AudioClip hiSelectfx;
-    AudioClip selectfx;
-    AudioClip backfx;
+    public AudioClip hiSelectfx;
+    public AudioClip selectfx;
+    public AudioClip backfx;
 
     // Use this for initialization
     void Start()
     {
-        animataC = GetComponent<Animator>();
-        defaultBehav = GetComponent<DialogControl>().closeDialog;
-        title = transform.Find("DialogBG").transform.Find("DialogTitle").GetComponent<Text>();
-        body = transform.Find("DialogBG").transform.Find("DialogText").GetComponent<Text>();
-        buttonL = transform.Find("DialogBG").transform.Find("ButtonL").gameObject;
-        buttonM = transform.Find("DialogBG").transform.Find("ButtonM").gameObject;
-        buttonR = transform.Find("DialogBG").transform.Find("ButtonR").gameObject;
-        buttonLText = transform.Find("DialogBG").transform.Find("ButtonL").transform.Find("Text").GetComponent<Text>();
-        buttonMText = transform.Find("DialogBG").transform.Find("ButtonM").transform.Find("Text").GetComponent<Text>();
-        buttonRText = transform.Find("DialogBG").transform.Find("ButtonR").transform.Find("Text").GetComponent<Text>();
+        //why hello there, preload the sounds
         hiSelectfx = Resources.Load<AudioClip>("Sounds/SFX/UI/hiSelect");
         selectfx = Resources.Load<AudioClip>("Sounds/SFX/UI/select");
         backfx = Resources.Load<AudioClip>("Sounds/SFX/UI/back");
-        buttonLSFX = transform.Find("DialogBG").transform.Find("ButtonL").transform.Find("SFX").GetComponent<AudioSource>();
-        buttonMSFX = transform.Find("DialogBG").transform.Find("ButtonM").transform.Find("SFX").GetComponent<AudioSource>();
-        buttonRSFX = transform.Find("DialogBG").transform.Find("ButtonR").transform.Find("SFX").GetComponent<AudioSource>();
     }
-
+    #region setDialog_doc
     /// <summary>
     /// Sets the data and actions for this dialog box.
     /// </summary>
+    ///
     /// <param name="titleS">The title of the dialog.</param>
     /// <param name="textS">The title of the dialog.</param>
-    /// <param name="buttons">Button types. 0 for only left. 1 for left and right. 2 for all three buttons.</param>
+    /// <param name="buttons">Button types. 0 for only left. 1 for middle and right. 2 for all three buttons.</param>
     /// <param name="scriptleft">UnityAction to call for the left button.</param>
     /// <param name="scriptmiddle">UnityAction to call for the middle button.</param>
     /// <param name="scriptright">UnityAction to call for the right button.</param>
     /// <param name="buttonLS">Text of the left button.</param>
     /// <param name="buttonMS">Text of the middle button.</param>
     /// <param name="buttonRS">Text of the right button.</param>
+    #endregion
     public void setDialog(string titleS, string textS, byte buttons, UnityAction scriptleft, UnityAction scriptmiddle,
                           UnityAction scriptright, AudioClip buttonLASFX, AudioClip buttonMASFX, AudioClip buttonRASFX,
                           string buttonLS = "", string buttonRS = "", string buttonMS = "")
     {
+        //declare every variable at the same time we tell the dialog to start
+        animataC = GetComponent<Animator>();
+        buttonL = transform.Find("DialogBG").transform.Find("ButtonL").gameObject;
+        buttonM = transform.Find("DialogBG").transform.Find("ButtonM").gameObject;
+        buttonR = transform.Find("DialogBG").transform.Find("ButtonR").gameObject;
+        defaultBehav = GetComponent<DialogControl>().closeDialog;
+        title = transform.Find("DialogBG").transform.Find("DialogTitle").GetComponent<Text>();
+        body = transform.Find("DialogBG").transform.Find("DialogText").GetComponent<Text>();
+        buttonLText = buttonR.transform.Find("Text").GetComponent<Text>();
+        buttonMText = buttonM.transform.Find("Text").GetComponent<Text>();
+        buttonRText = buttonR.transform.Find("Text").GetComponent<Text>();
+        buttonLSFX = buttonL.transform.Find("SFX").GetComponent<AudioSource>();
+        buttonMSFX = buttonM.transform.Find("SFX").GetComponent<AudioSource>();
+        buttonRSFX = buttonR.transform.Find("SFX").GetComponent<AudioSource>();
+
+        //set the sound effects
+        buttonLSFX.clip = buttonLASFX;
+        buttonMSFX.clip = buttonMASFX;
+        buttonRSFX.clip = buttonRASFX;
+
+        //set the dialog data
         title.text = titleS;
         body.text = textS;
         buttonLText.text = buttonLS;
         buttonMText.text = buttonMS;
         buttonRText.text = buttonRS;
+
+        //set button layout
+        // __ __ ##
         if (buttons == 0)
         {
             buttonL.SetActive(false);
             buttonM.SetActive(false);
             buttonR.SetActive(true);
         }
+        // __ ## ##
         else if (buttons == 1)
         {
-            buttonL.SetActive(true);
-            buttonM.SetActive(false);
+            buttonL.SetActive(false);
+            buttonM.SetActive(true);
             buttonR.SetActive(true);
         }
+        // ## ## ##
         else if (buttons == 2)
         {
             buttonL.SetActive(true);
             buttonM.SetActive(true);
             buttonR.SetActive(true);
         }
+        // if invalid: __ __ __
         else
         {
             Debug.Log("Invalid button set attempted. The dialog with the title \"" + titleS + "\" will show no buttons and will be rendered unusable.");
@@ -90,31 +107,18 @@ public class DialogControl : MonoBehaviour
             buttonM.SetActive(false);
             buttonR.SetActive(false);
         }
+        //assign the correspondent scripts
         buttonL.GetComponent<Button>().onClick.AddListener(scriptleft);
         buttonR.GetComponent<Button>().onClick.AddListener(scriptright);
         buttonM.GetComponent<Button>().onClick.AddListener(scriptmiddle);
-        if (buttonMASFX == null || buttonLASFX == null || buttonRASFX == null)
-        {
-            Debug.Log("Sounds were incorrectly assigned.");
-        }
-        else
-        {
-            buttonLSFX.clip = buttonLASFX;
-            buttonMSFX.clip = buttonMASFX;
-            buttonRSFX.clip = buttonRASFX;
-        }
         animataC.Play("Enter");
     }
 
     public void closeDialog()
     {
         animataC.Play("Exit");
-        Destroy(gameObject, 1f);
+        // all UI SFX should be no more than 1.5s.
+        Destroy(gameObject, 1.5f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
---
+++ Assets/Scripts/UI/startDemoDialog.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class startDemoDialog : MonoBehaviour {
+
+    GameObject dialogObj;
+    GameObject dialogPref;
+
+    // Use this for initialization
+    void Start () {
+        
+        dialogPref =  Resources.Load<GameObject>("Prefabs/UI/Dialog");
+        dialogObj = Instantiate(dialogPref);
+        dialogObj.transform.SetParent(GameObject.FindGameObjectWithTag("UICanvas").transform, false);
+        dialogObj.SetActive(true);
+        StartDemoDialogAfter();
+    }
+	
+	// Update is called once per frame
+	void StartDemoDialogAfter() {
+        dialogObj.GetComponent<DialogControl>().setDialog(
+                                            "Demo",
+                                            "Remember this is a demo. Any error or missing thing should be reported to me in the respective channel.",
+                                            0,
+                                            null,
+                                            null,
+                                            dialogObj.GetComponent<DialogControl>().closeDialog,
+                                            null,
+                                            null,
+                                            Resources.Load<AudioClip>("Sounds/SFX/UI/hiSelection"), "", "OK", "");
+    }
+}

86e6b09e092033475985d6d8a78da86e9117db70
GlitchyPSIX <supermglitchy3@gmail.com>
2017-12-29 12:27:37 -0400

    Add SFX placeholders, kinda sh*tty SFX for dialog

--- Assets/Scripts/UI/DialogControl.cs
+++ Assets/Scripts/UI/DialogControl.cs
@@ -4,86 +4,117 @@ using UnityEngine.Events;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class DialogControl : MonoBehaviour {
+public class DialogControl : MonoBehaviour
+{
     Animator animataC;
     UnityAction defaultBehav;
     Text title;
     Text body;
     Text buttonLText;
-
     Text buttonMText;
     Text buttonRText;
     GameObject buttonL;
-	GameObject buttonM;
-	GameObject buttonR;
+    GameObject buttonM;
+    GameObject buttonR;
+    AudioSource buttonLSFX;
+    AudioSource buttonMSFX;
+    AudioSource buttonRSFX;
+    AudioClip hiSelectfx;
+    AudioClip selectfx;
+    AudioClip backfx;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         animataC = GetComponent<Animator>();
         defaultBehav = GetComponent<DialogControl>().closeDialog;
         title = transform.Find("DialogBG").transform.Find("DialogTitle").GetComponent<Text>();
-		body = transform.Find("DialogBG").transform.Find("DialogText").GetComponent<Text>();
+        body = transform.Find("DialogBG").transform.Find("DialogText").GetComponent<Text>();
         buttonL = transform.Find("DialogBG").transform.Find("ButtonL").gameObject;
-		buttonM = transform.Find("DialogBG").transform.Find("ButtonM").gameObject;
-		buttonR = transform.Find("DialogBG").transform.Find("ButtonR").gameObject;
+        buttonM = transform.Find("DialogBG").transform.Find("ButtonM").gameObject;
+        buttonR = transform.Find("DialogBG").transform.Find("ButtonR").gameObject;
         buttonLText = transform.Find("DialogBG").transform.Find("ButtonL").transform.Find("Text").GetComponent<Text>();
-		buttonMText = transform.Find("DialogBG").transform.Find("ButtonM").transform.Find("Text").GetComponent<Text>();
-		buttonRText = transform.Find("DialogBG").transform.Find("ButtonR").transform.Find("Text").GetComponent<Text>();
+        buttonMText = transform.Find("DialogBG").transform.Find("ButtonM").transform.Find("Text").GetComponent<Text>();
+        buttonRText = transform.Find("DialogBG").transform.Find("ButtonR").transform.Find("Text").GetComponent<Text>();
+        hiSelectfx = Resources.Load<AudioClip>("Sounds/SFX/UI/hiSelect");
+        selectfx = Resources.Load<AudioClip>("Sounds/SFX/UI/select");
+        backfx = Resources.Load<AudioClip>("Sounds/SFX/UI/back");
+        buttonLSFX = transform.Find("DialogBG").transform.Find("ButtonL").transform.Find("SFX").GetComponent<AudioSource>();
+        buttonMSFX = transform.Find("DialogBG").transform.Find("ButtonM").transform.Find("SFX").GetComponent<AudioSource>();
+        buttonRSFX = transform.Find("DialogBG").transform.Find("ButtonR").transform.Find("SFX").GetComponent<AudioSource>();
     }
 
-	/// <summary>
+    /// <summary>
     /// Sets the data and actions for this dialog box.
     /// </summary>
     /// <param name="titleS">The title of the dialog.</param>
     /// <param name="textS">The title of the dialog.</param>
-	/// <param name="buttons">Button types. 0 for only left. 1 for left and right. 2 for all three buttons.</param>
-	/// <param name="scriptleft">UnityAction to call for the left button.</param>
-	/// <param name="scriptmiddle">UnityAction to call for the middle button.</param>
-	/// <param name="scriptright">UnityAction to call for the right button.</param>
-	/// <param name="buttonLS">Text of the left button.</param>
-	/// <param name="buttonMS">Text of the middle button.</param>
-	/// <param name="buttonRS">Text of the right button.</param>
+    /// <param name="buttons">Button types. 0 for only left. 1 for left and right. 2 for all three buttons.</param>
+    /// <param name="scriptleft">UnityAction to call for the left button.</param>
+    /// <param name="scriptmiddle">UnityAction to call for the middle button.</param>
+    /// <param name="scriptright">UnityAction to call for the right button.</param>
+    /// <param name="buttonLS">Text of the left button.</param>
+    /// <param name="buttonMS">Text of the middle button.</param>
+    /// <param name="buttonRS">Text of the right button.</param>
     public void setDialog(string titleS, string textS, byte buttons, UnityAction scriptleft, UnityAction scriptmiddle,
-                    UnityAction scriptright, string buttonLS = "", string buttonRS = "", string buttonMS = ""){
+                          UnityAction scriptright, AudioClip buttonLASFX, AudioClip buttonMASFX, AudioClip buttonRASFX,
+                          string buttonLS = "", string buttonRS = "", string buttonMS = "")
+    {
         title.text = titleS;
         body.text = textS;
         buttonLText.text = buttonLS;
-		buttonMText.text = buttonMS;
-		buttonRText.text = buttonRS;
-        if (buttons == 0){
+        buttonMText.text = buttonMS;
+        buttonRText.text = buttonRS;
+        if (buttons == 0)
+        {
             buttonL.SetActive(false);
-			buttonM.SetActive(false);
-			buttonR.SetActive(true);
+            buttonM.SetActive(false);
+            buttonR.SetActive(true);
         }
-		else if (buttons == 1){
-			buttonL.SetActive(true);
-			buttonM.SetActive(false);
-			buttonR.SetActive(true);
-		}
-		else if (buttons == 2){
-			buttonL.SetActive(true);
-			buttonM.SetActive(true);
-			buttonR.SetActive(true);
-		}
-		else{
+        else if (buttons == 1)
+        {
+            buttonL.SetActive(true);
+            buttonM.SetActive(false);
+            buttonR.SetActive(true);
+        }
+        else if (buttons == 2)
+        {
+            buttonL.SetActive(true);
+            buttonM.SetActive(true);
+            buttonR.SetActive(true);
+        }
+        else
+        {
             Debug.Log("Invalid button set attempted. The dialog with the title \"" + titleS + "\" will show no buttons and will be rendered unusable.");
             buttonL.SetActive(false);
-			buttonM.SetActive(false);
-			buttonR.SetActive(false);
-		}
+            buttonM.SetActive(false);
+            buttonR.SetActive(false);
+        }
         buttonL.GetComponent<Button>().onClick.AddListener(scriptleft);
-		buttonR.GetComponent<Button>().onClick.AddListener(scriptright);
-		buttonM.GetComponent<Button>().onClick.AddListener(scriptmiddle);
+        buttonR.GetComponent<Button>().onClick.AddListener(scriptright);
+        buttonM.GetComponent<Button>().onClick.AddListener(scriptmiddle);
+        if (buttonMASFX == null || buttonLASFX == null || buttonRASFX == null)
+        {
+            Debug.Log("Sounds were incorrectly assigned.");
+        }
+        else
+        {
+            buttonLSFX.clip = buttonLASFX;
+            buttonMSFX.clip = buttonMASFX;
+            buttonRSFX.clip = buttonRASFX;
+        }
         animataC.Play("Enter");
     }
 
-	public void closeDialog(){
+    public void closeDialog()
+    {
         animataC.Play("Exit");
-        Destroy(gameObject, 0.5f);
+        Destroy(gameObject, 1f);
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }

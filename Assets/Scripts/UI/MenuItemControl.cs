41d6ef8c49c2ceb570542b4e6f89c32fda26a9b8
GlitchyPSIX <supermglitchy3@gmail.com>
2017-12-29 22:53:15 -0400

    Further progress on the menu items.

--- Assets/Scripts/UI/MenuItemControl.cs
+++ Assets/Scripts/UI/MenuItemControl.cs
@@ -7,24 +7,43 @@ using UnityEngine;
 
 public class MenuItemControl : MonoBehaviour {
 
-    Sprite menuSprite;
+    Animator animataM;
+    Image menuSprite;
+    GameObject menuBtn;
     Text menuText;
     string menuSubtitleText;
+    public Sprite uiIcons;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        
+        }
 
     void OnMouseEnter()
     {
         //If your mouse hovers over the GameObject with the script attached, output this message
-        Debug.Log("Mouse is over GameObject.");
+        animataM.Play("menuHover");
+    }
+
+    void OnMouseExit(){
+        animataM.Play("menuOut");
     }
 
     public void setMenuItem(string titleS, string subtitle, Sprite spriteS, UnityAction script, AudioClip buttonSFX){
+        animataM = GetComponent<Animator>();
+        menuSprite = transform.Find("MenuBG").gameObject.transform.Find("Image").GetComponent<Image>();
+        menuText = transform.Find("MenuBG").gameObject.transform.Find("Text").GetComponent<Text>();
+        menuBtn = transform.Find("MenuBG").gameObject;
+        menuSprite.sprite = spriteS;
+        menuText.text = titleS;
+        menuBtn.GetComponent<Button>().onClick.AddListener(script);
+        animataM.Play("menuComeIn");
+     }
 
+	public void killItem()
+    {
+        animataM.Play("menuHide");
+        // all UI SFX should be no more than 1.5s.
+        Destroy(gameObject, 1.5f);
     }
-
-	
 }
--- Assets/Scripts/UI/startDemoDialog.cs
+++ Assets/Scripts/UI/startDemoDialog.cs
@@ -1,23 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class startDemoDialog : MonoBehaviour {
+public class startDemoDialog : MonoBehaviour
+{
 
     GameObject dialogObj;
     GameObject dialogPref;
+    GameObject menuObj;
+    GameObject menuPref;
 
     // Use this for initialization
-    void Start () {
-        
-        dialogPref =  Resources.Load<GameObject>("Prefabs/UI/Dialog");
+    void Start()
+    {
+
+        dialogPref = Resources.Load<GameObject>("Prefabs/UI/Dialog");
+        menuPref = Resources.Load<GameObject>("Prefabs/UI/MenuItem");
         dialogObj = Instantiate(dialogPref);
+        menuObj = Instantiate(menuPref);
         dialogObj.transform.SetParent(GameObject.FindGameObjectWithTag("UICanvas").transform, false);
+        menuObj.transform.SetParent(GameObject.FindGameObjectWithTag("UIMenuContainer").transform, false);
         dialogObj.SetActive(true);
         StartDemoDialogAfter();
     }
-	
-	// Update is called once per frame
-	void StartDemoDialogAfter() {
+
+    // Update is called once per frame
+    void StartDemoDialogAfter()
+    {
+        menuObj.GetComponent<MenuItemControl>().setMenuItem("Play Demo", "Play this demo.", Resources.Load<Sprite>("Sprites/particleStar"), menuObj.GetComponent<MenuItemControl>().killItem, Resources.Load<AudioClip>("Sounds/SFX/UI/hiSelection"));
+
         dialogObj.GetComponent<DialogControl>().setDialog(
                                             "Demo",
                                             "Remember this is a demo. Any error or missing thing should be reported to me in the respective channel.",

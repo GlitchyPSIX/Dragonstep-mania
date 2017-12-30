e2d908fbe25ea8f19864b3cebc1375ebab3c76fc
GlitchyPSIX <supermglitchy3@gmail.com>
2017-12-29 21:32:46 -0400

    Start work on main menu

---
+++ Assets/Scripts/UI/MenuItemControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class MenuItemControl : MonoBehaviour {
+
+    Sprite menuSprite;
+    Text menuText;
+    string menuSubtitleText;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void OnMouseEnter()
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        Debug.Log("Mouse is over GameObject.");
+    }
+
+    public void setMenuItem(string titleS, string subtitle, Sprite spriteS, UnityAction script, AudioClip buttonSFX){
+
+    }
+
+	
+}

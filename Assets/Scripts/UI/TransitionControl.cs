600632e5843bfefe7fd7b402dffb75282473fcb9
GlitchyPSIX <supermglitchy3@gmail.com>
2018-02-07 15:00:56 -0400

    Work on Transitions (PART 1)

---
+++ Assets/Scripts/UI/TransitionControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TransitionControl : MonoBehaviour
+{
+
+    Sprite[] transitionFaces;
+    Animator ac;
+    Image maskimage;
+
+    // Use this for initialization
+    void Start()
+    {
+        transitionFaces = Resources.LoadAll<Sprite>("Sprites/UI/TransitionMasks");
+        ac = GetComponent<Animator>();
+        maskimage = transform.Find("Mask").GetComponent<Image>();
+    }
+
+    public void PlayTransition(int face, bool comingIn)
+    {
+        maskimage.sprite = transitionFaces[face];
+        if (comingIn == true)
+        {
+            ac.Play("transitionIn");
+        }
+        else
+        {
+            ac.Play("transitionOut");
+        }
+    }
+}
---
+++ Assets/TryTransitions.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TryTransitions : MonoBehaviour {
+    TransitionControl tc;
+	// Use this for initialization
+	void Start () {
+        tc = GetComponent<TransitionControl>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("a"))
+        {
+            if (Input.GetKey("q"))
+            {
+                tc.PlayTransition(0, true);
+            }
+            else if (Input.GetKey("w"))
+            {
+                tc.PlayTransition(1, true);
+            }
+            else if (Input.GetKey("e"))
+            {
+                tc.PlayTransition(2, true);
+            }
+            else if (Input.GetKey("r"))
+            {
+                tc.PlayTransition(3, true);
+            }
+            else if (Input.GetKey("t"))
+            {
+                tc.PlayTransition(4, true);
+            }
+        }
+        else if (Input.GetKey("s"))
+        {
+            if (Input.GetKey("q"))
+            {
+                tc.PlayTransition(0, false);
+            }
+            else if (Input.GetKey("w"))
+            {
+                tc.PlayTransition(1, false);
+            }
+            else if (Input.GetKey("e"))
+            {
+                tc.PlayTransition(2, false);
+            }
+            else if (Input.GetKey("r"))
+            {
+                tc.PlayTransition(3, false);
+            }
+            else if (Input.GetKey("t"))
+            {
+                tc.PlayTransition(4, false);
+            }
+        }
+	}
+}
